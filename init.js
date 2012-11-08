5f7a15d8a9e24bd7a29b3a709f278b5f926a4df3
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2012-11-08 17:31:44 -0500

    Moving to 2d context

--- init.js
+++ init.js
@@ -1,32 +1,7 @@
 
-function start() {
-  var canvas = document.getElementById("glcanvas");
- 
-  initWebGL(canvas);      // Initialize the GL context
-   
-  // Only continue if WebGL is available and working
-   
-  if (gl) {
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);                      // Set clear color to black, fully opaque
-    gl.enable(gl.DEPTH_TEST);                               // Enable depth testing
-    gl.depthFunc(gl.LEQUAL);                                // Near things obscure far things
-    gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);      // Clear the color as well as the depth buffer.
-  }
-}
-
-
-function initWebGL(canvas) {
-  // Initialize the global variable gl to null.
-  gl = null;
-   
-  try {
-    // Try to grab the standard context. If it fails, fallback to experimental.
-    gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-  }
-  catch(e) {}
-   
-  // If we don't have a GL context, give up now
-  if (!gl) {
-    alert("Unable to initialize WebGL. Your browser may not support it.");
-  }
-}
+function start() 
+    {
+    var canvas = document.getElementById("canvas2d");
+    var ctx = canvas.getContext("2d");
+    ctx.fillRect(50, 25, 150, 100);
+    }
