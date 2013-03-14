fe9a35e2ec50dcc92bde215c175babf8978b676d
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2013-03-14 17:42:40 -0400

    Inheritance working

--- Init.js
+++ Init.js
@@ -3,9 +3,9 @@ function start()
     {
     var canvas = document.getElementById("canvas2d");
     var ctx = canvas.getContext("2d");
-    sim = new Simulation(10,10);
+    sim = new SynapticRearrangement(10,10, 3);
     sim.Init();
-    
+    console.log(sim)
     window.setInterval(function() {
             sim.Animate(ctx);
             }, 100);
--- SynapticRearrangement.js
+++ SynapticRearrangement.js
@@ -1,3 +1,15 @@
 /**
  * @author dhanannjay.deo
  */
+
+
+// Inherit from Simulation class 
+SynapticRearrangement.prototype = new Simulation();
+SynapticRearrangement.constructor = SynapticRearrangement;
+ 
+function SynapticRearrangement( width, height, no_axons)
+    {
+    // Call super constructor.
+    Simulation.call( this, width, height );
+    this.no_axons = no_axons;
+    }
