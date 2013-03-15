1ef80ebf5ab77f181e6c2c577afa8f7da84fec2a
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2013-03-15 12:20:27 -0400

    Creating multiple junctions in a loop

--- Init.js
+++ Init.js
@@ -5,7 +5,11 @@ function start()
     var sims = [];
     // Add two simulations
     // sims.push(new Simulation(10,10));
-    sims.push(new SynapticRearrangement(10,10, 3));
+    for(var a = 0; a < 100; a ++)
+        {
+        sims.push(new SynapticRearrangement(10,10, 10));
+            
+        }
 
     window.setInterval(function() {
             for (var i = 0; i < sims.length; i++) 
